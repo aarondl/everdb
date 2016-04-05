026b5ecafee346ff6dfafd1aa54f37f5caea178a
Aaron L <aaron@bettercoder.net>
2016-04-05 00:08:25 -0700

    Add get_bucket which can't do anything without set

--- src/hash/mod.rs
+++ src/hash/mod.rs
@@ -15,19 +15,61 @@
 #[macro_use]
 mod entry;
 
+use std::collections::HashMap;
+
+use self::entry::SubBucketer;
+
 const HASH_FOOTER_SIZE : u32 = 64 + 32 + 8; // sizeof(Hash)
 
-/*
 /// Hash is a linear hash table
 struct Hash {
     count: u64,
     split: u32,
     level: u8,
+
+    blocks: Vec<SubBucketer>,
 }
+
+const SUB_BUCKET_BITS : u32 = 8;
+const SUB_BUCKET      : u32 = (1 << SUB_BUCKET_BITS); //(256)
+const SUB_BUCKET_MASK : u32 = (SUB_BUCKET - 1);       //(255 (1111 1111))
+
+type MaybeHash = Option<HashMap<i32,i32>>;
 
 impl Hash {
     fn new() -> Hash {
-        Hash { count: 0, split: 0, level: 0 }
+        Hash {
+            count: 1,
+            split: 0,
+            level: 1,
+            blocks: vec![SubBucketer::new()],
+        }
+    }
+
+    fn get_bucket(&self, key : String) -> (u8, MaybeHash) {
+        let hash : u32 = key.as_bytes().iter().fold(0, |a, b| a + (*b as u32)); // "hash" the string LOL
+
+        let level = 1 << self.level;
+        // h & (256)^(level+1) - 1  -- truncate the hash value to bits under some power of 2 based on level
+        let mut bucket = hash & ((SUB_BUCKET << 1 << level) - 1);
+
+        // (2^level + self.split)^8 -- check to ensure that the bucket fits within the
+        // parameters of level and split if not we'll truncate it again
+        if bucket >= ((1 << level) + self.split) << SUB_BUCKET_BITS {
+            // truncate the hash value to bits under one less power of 2 of the level than before
+            bucket = hash & ((SUB_BUCKET << level) - 1);
+        }
+
+        // Truncate again to ensure we fall within our sub bucket indexing
+        let sub_bucket_index = (bucket & SUB_BUCKET_MASK) as u8;
+        // Divide by 2^8 - this is effectively a % operation to ensure we select the right
+        // block, but the modding is mostly done above. This succeeds because count = level + split
+        let block = &self.blocks[(bucket >> SUB_BUCKET_BITS) as usize];
+
+        match block.get_sub_bucket(sub_bucket_index) {
+            Ok(x)  => return (sub_bucket_index, x),
+            Err(x) => panic!("how can there be no sub_bucket here?".to_string() + &x.to_string()),
+        }
     }
 }
 
@@ -37,9 +79,14 @@ mod tests {
     fn new() {
         let h = super::Hash::new();
 
-        assert!(h.count == 0);
-        assert!(h.split == 0);
-        assert!(h.level == 0);
+        assert_eq!(h.count, 1);
+        assert_eq!(h.split, 0);
+        assert_eq!(h.level, 1);
+        assert_eq!(h.blocks.len(), 1);
+    }
+
+    #[test]
+    fn get_bucket() {
+        let h = super::Hash::new();
     }
 }
-*/
